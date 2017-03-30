172ca231183a976731af26b8fa8baea42f3f7424
kean0048 <rh_king@163.com>
2017-03-30 09:47:55 +0800

    remove tb

--- tb/ZCS-ROBOT(BD)-RECV_V3.0/STM8_RECV_V1.6(R)/USER/Stream2Char.h
+++
@@ -1,9 +0,0 @@
-
-#include "TypeDefs.h"
-#include "DataStruct.h"
-//#include "SaveLog.h"
-void iF2IDsp1(TMdII mdII[], int cIP, unsigned char *frm);
-
-unsigned int crc_cal_value(unsigned char *data_value, unsigned char data_length);
-void frameStream2SRFrameDSsp1();
-	
--- tb/ZCS-ROBOT(BD)-RECV_V3.0/STM8_RECV_V3.0/USER/delayTime.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DELAYTIME_H
-#define _DELAYTIME_H
-
-void _delay_ms(unsigned long ms);
-void  _delay_1_ms(void);
-void _delay_us(unsigned long us);
-
-#endif
